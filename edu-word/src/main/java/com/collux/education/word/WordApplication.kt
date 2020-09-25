cf778747dbe76136f7231e2caa467fbfd48d11d3
lusiyi <lusiyi@szboanda.net>
2020-09-25 19:14:15 +0800

    fix

--- edu-word/src/main/java/com/collux/education/word/WordApplication.kt
+++ edu-word/src/main/java/com/collux/education/word/WordApplication.kt
@@ -2,6 +2,7 @@ package com.collux.education.word
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 /**
  * 单词微服务应用入口
@@ -9,6 +10,7 @@ import org.springframework.boot.runApplication
  * @since 2020/9/18
  */
 @SpringBootApplication
+@EnableEurekaClient
 open class WordApplication
 
 fun main(args: Array<String>) {
